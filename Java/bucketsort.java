7cee086a61475e6a13d9f3bb6957fe4995d53527
Percival Tapera <perctapera@gmail.com>
2022-10-08 08:18:04 +0200

    Update bucketsort.java

--- Java/bucketsort.java
+++ Java/bucketsort.java
@@ -5,12 +5,12 @@ public static int[] bucketsort(int[] array, int maximum_value)
 {
 //creating an empty array called newbucket which is considered as bucket array
 int[] newbucket = new int[maximum_value + 1];
-//creating another empty array called sorted_array to store the result array
+//creating another empty array called sorted_array to store the result array.
 int[] sorted_array = new int[array.length];
-//traversing through the input array to add each element to the bucket array
+//traversing through the input array to add each element to the bucket array.
 for (int a= 0; a <array.length; a++)
 newbucket[array[a]]++;
-//sorting each element in the bucket array and adding each sorted element in order to the original input array
+//sorting each element in the bucket array and adding each sorted element in order to the original input array.
 int position = 0;
 for (int b = 0; b < newbucket.length; b++)
 for (int c = 0; c < newbucket[b]; c++)
